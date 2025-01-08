c63437dd326c05c9bd0aed56ff8de53ac778f921
Sankhadip Das <sankhadip10.das@gmail.com>
2025-01-08 21:26:58 +0530

    Leetcode Problem  brute force and optimal solution 6th  jan

--- Leetcode/2025/6th_Jan.py
+++ Leetcode/2025/6th_Jan.py
@@ -1,24 +1,91 @@
+"""
+You have n boxes. You are given a binary string boxes of length n, where boxes[i] is '0' if the ith box is empty, and '1' if it contains one ball.
+
+In one operation, you can move one ball from a box to an adjacent box. Box i is adjacent to box j if abs(i - j) == 1. Note that after doing so, there may be more than one ball in some boxes.
+
+Return an array answer of size n, where answer[i] is the minimum number of operations needed to move all the balls to the ith box.
+
+Each answer[i] is calculated considering the initial state of the boxes.
+
+Example 1:
+Input: boxes = "110"
+Output: [1,1,3]
+Explanation: The answer for each box is as follows:
+1) First box: you will have to move one ball from the second box to the first box in one operation.
+2) Second box: you will have to move one ball from the first box to the second box in one operation.
+3) Third box: you will have to move one ball from the first box to the third box in two operations, and move one ball from the second box to the third box in one operation.
+
+Example 2:
+Input: boxes = "001011"
+Output: [11,8,5,4,3,4]
+
+Constraints:
+- n == boxes.length
+- 1 <= n <= 2000
+- boxes[i] is either '0' or '1'.
+"""
+from typing import List
+
+
+# from typing import List
+# class Solution:
+#     def minOperationsBruteForce(self, boxes:str)->List[int]:
+#         n=len(boxes)
+#         ans = [0]* n
+#         for i in range(n):
+#             operations = 0
+#             for j in range(n):
+#                 if boxes[j] == '1':
+#                     operations +=abs(j-i)
+#             ans[i] = operations
+#
+#         return ans
+#
+# s = Solution()
+# print(s.minOperationsBruteForce(boxes = "110"))
+
+# Time and Space Complexity:
+# Time Complexity: O(n²) because for each of the n boxes, we iterate over all n boxes.
+#
+# Space Complexity: O(n) for storing the answer array.
+
+
+# from typing import List
+# class Solution:
+#     def minOperationsBruteForce(self, boxes:str)->List[int]:
+
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         n = len(boxes)
-        distances = [0] * n
+        left = [0]*n
+        right = [0]*n
 
-        prefixCount = 0
-        prefixSum = 0
+        count = 0
+        total = 0
 
-        for i in range(n):
-            distances[i] = prefixCount * i - prefixSum
-            if boxes[i] == '1':
-                prefixCount += 1
-                prefixSum += i
+        #left
+        for i in range(1,n):
+            if boxes[i-1] == '1':
+                count +=1
+            total += count
+            left[i] = total
 
-        suffixCount = 0
-        suffixSum = 0
+        #right
+        count = 0
+        total = 0
+        for j in range(n-2,-1,-1):
+            if boxes[j+1] == '1':
+                count +=1
+            total += count
+            right[j] = total
 
-        for i in range(n - 1, -1, -1):
-            distances[i] += suffixSum - suffixCount * i
-            if boxes[i] == '1':
-                suffixCount += 1
-                suffixSum += i
+        answer = [left[i]+right[i] for i in range(n)]
+        return answer
 
-        return distances
+s = Solution()
+print(s.minOperations("001011"))
+
+
+# Time Complexity: O(n) because we only iterate over the array a few times.
+#
+# Space Complexity: O(n) for storing the left, right, and answer arrays.
