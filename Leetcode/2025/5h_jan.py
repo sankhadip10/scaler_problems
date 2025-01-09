b27972e279fd8a0e43ed8cc522442f55c433b22b
Sankhadip Das <sankhadip10.das@gmail.com>
2025-01-09 23:51:15 +0530

    Leetcode Problem  brute force and optimal solution 9th  jan

--- Leetcode/2025/5h_jan.py
+++ Leetcode/2025/5h_jan.py
@@ -1,19 +1,36 @@
+# class Solution:
+#     def shiftingLetters(self, s: str, shifts: list[list[int]]) -> str:
+#         n = len(s)
+#         shift = [0] * (n + 1)
+#
+#         for shiftOp in shifts:
+#             start, end, direction = shiftOp
+#             shift[start] += (1 if direction == 1 else -1)
+#             if end + 1 < n:
+#                 shift[end + 1] -= (1 if direction == 1 else -1)
+#
+#         currentShift = 0
+#         shiftList = list(s)
+#         for i in range(n):
+#             currentShift += shift[i]
+#             netShift = (currentShift % 26 + 26) % 26
+#             shiftList[i] = chr((ord(shiftList[i]) - ord('a') + netShift) % 26 + ord('a'))
+#
+#         return ''.join(shiftList)
+
+
 class Solution:
-    def shiftingLetters(self, s: str, shifts: list[list[int]]) -> str:
-        n = len(s)
-        shift = [0] * (n + 1)
+    def shift(self,c,direction):
+        if direction == 1:
+            return chr(ord('a') + (ord(c) - ord('a') + 1) % 26)
+        else:
+            return chr(ord(c) + (ord('c') - ord('a') - 1) % 26)
 
-        for shiftOp in shifts:
-            start, end, direction = shiftOp
-            shift[start] += (1 if direction == 1 else -1)
-            if end + 1 < n:
-                shift[end + 1] -= (1 if direction == 1 else -1)
 
-        currentShift = 0
-        shiftList = list(s)
-        for i in range(n):
-            currentShift += shift[i]
-            netShift = (currentShift % 26 + 26) % 26
-            shiftList[i] = chr((ord(shiftList[i]) - ord('a') + netShift) % 26 + ord('a'))
 
-        return ''.join(shiftList)
+    def shiftingLettersBruteForce(self,s:str,shifts:list[list[int]]) -> str:
+        new_c = list(s)
+        for start,end,direction in shifts:
+            for i in range()
+
+        return ''
