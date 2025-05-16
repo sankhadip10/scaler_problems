ca2ab71eface14bc89b7face5a6752b880bbc4e8
Sankhadip Das <121531893+sankhadip10@users.noreply.github.com>
2025-05-16 23:55:54 +0530

    Create 16thMay.py

---
+++ Leetcode/2025/16thMay.py
@@ -0,0 +1,24 @@
+class Solution:
+    def getWordsInLongestSubsequence(self, words, groups):
+        from functools import lru_cache
+
+        def hamming_dist(w1, w2):
+            return sum(a != b for a, b in zip(w1, w2)) == 1
+
+        n = len(words)
+        @lru_cache(None)
+        def dfs(i, last_group, last_word):
+            if i == n:
+                return []
+            take = []
+            if len(words[i]) == len(last_word) and hamming_dist(last_word, words[i]) and groups[i] != last_group:
+                take = [words[i]] + dfs(i + 1, groups[i], words[i])
+            skip = dfs(i + 1, last_group, last_word)
+            return take if len(take) > len(skip) else skip
+
+        res = []
+        for i in range(n):
+            curr = [words[i]] + dfs(i + 1, groups[i], words[i])
+            if len(curr) > len(res):
+                res = curr
+        return res
